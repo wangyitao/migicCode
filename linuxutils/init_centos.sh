12ee1b97f4b45502c7d904fe650f9e7684019653
felix <1403179190@qq.com>
2019-07-14 19:05:00 +0800

    增加数据库安装以及优化代码

--- linuxutils/init_centos.sh
+++ linuxutils/init_centos.sh
@@ -7,21 +7,42 @@ yum -y update
 yum -y groupinstall "Development tools"
 yum -y install java-1.8.0-openjdk java-1.8.0-openjdk-devel zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel libffi-devel
 yum -y install wget vim net-tools
-rm -rf /var/cache/yum/*
-wget https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tar.xz
-if [ -d $INSTALL_DIR ];then
-    rm -rf $INSTALL_DIR
+wget https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-$PYTHON_VERSION.tar.xz
+if [[ -d ${INSTALL_DIR} ]];then
+    rm -rf ${INSTALL_DIR}
 fi
-mkdir $INSTALL_DIR
-mv Python-$PYTHON_VERSION.tar.xz $INSTALL_DIR
-cd $INSTALL_DIR
-tar -xvJf Python-$PYTHON_VERSION.tar.xz
-rm -rf Python-$PYTHON_VERSION.tar.xz && cd Python-$PYTHON_VERSION
-./configure --prefix=$INSTALL_DIR
+mkdir ${INSTALL_DIR}
+mv Python-${PYTHON_VERSION}.tar.xz ${INSTALL_DIR}
+cd ${INSTALL_DIR}
+tar -xvJf Python-${PYTHON_VERSION}.tar.xz
+rm -rf Python-${PYTHON_VERSION}.tar.xz && cd Python-$PYTHON_VERSION
+./configure --prefix=${INSTALL_DIR}
 make && make install && make distclean
-rm -rf $INSTALL_DIR/Python-$PYTHON_VERSION/
-cd $INSTALL_DIR/bin/
-./pip3 install --no-cache-dir --upgrade pip -i https://pypi.tuna.tsinghua.edu.cn/simple
-./pip3 install --no-cache-dir pipenv -i https://pypi.tuna.tsinghua.edu.cn/simple
-echo "export PATH=${INSTALL_DIR}/bin/:$PATH" >> ~/.bashrc
+rm -rf ${INSTALL_DIR}/Python-${PYTHON_VERSION}/
+if [[ -d ~/.pip ]];then
+    rm -rf ~/.pip
+fi
+mkdir ~/.pip
+echo "[global]
+    index-url = https://pypi.mirrors.ustc.edu.cn/simple/
+[install]
+    trusted-host=mirrors.aliyun.com " > ~/.pip/pip.conf
+cd ${INSTALL_DIR}/bin/
+./pip3 install --no-cache-dir --upgrade pip
+./pip3 install --no-cache-dir pipenv
+echo "export PATH=${INSTALL_DIR}/bin/:'$PATH'" >> ~/.bashrc
+echo "[mariadb]
+name = MariaDB
+baseurl = https://mirrors.ustc.edu.cn/mariadb/yum/10.4/centos7-amd64/
+gpgkey=https://mirrors.ustc.edu.cn/mariadb/yum/RPM-GPG-KEY-MariaDB
+gpgcheck=1" > /etc/yum.repos.d/mariadb.repo
+yum -y update
+yum -y install mariadb-server mariadb-client
+echo "[mysqld]
+character-set-server=utf8
+collation-server=utf8_unicode_ci
+skip-character-set-client-handshake" >> /etc/my.cnf
+systemctl start mariadb
+systemctl enable mariadb
+rm -rf /var/cache/yum/*
 reboot
